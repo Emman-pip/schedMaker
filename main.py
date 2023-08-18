fea76be4b42f2a150745da9c068f4fe76053453e
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2023-08-19 06:07:08 +0800

    Add conflict schedule checker.

--- main.py
+++ main.py
@@ -13,3 +13,34 @@ class Subject:
         self.isStartAM = isStartAM
         self.end = end
         self.isEndAM = isEndAM
+
+
+def conflictChecker(block1, block2="nothing"):
+    for key, value in block1.subjects.items():
+        for key2, value2 in block2.subjects.items():
+            # if value.day is the same:
+            if key != key2:
+                if value.day != value2.day:
+                    continue
+                # print(block2.subjects.keys())
+                # print(key in block2.subjects.keys() and key2 in block2.subjects.keys())
+                if value.isStartAM == value2.isStartAM:
+                    # TODO: if from the same block yung subject -> skip iteration
+                    if (
+                        value.start < value2.start < value.end
+                        or value2.start < value.start < value2.end
+                        or value.start < value2.end < value.end
+                        or value2.start < value.end < value2.end
+                        # or value.start == value.start
+                        or (value.start == value2.start and value.end == value2.end)
+                    ):
+                        print(
+                            f"Conflict of schedule:  {key} in block {block1.block} and {key2} in block:{block2.block}"
+                        )
+                        return
+                else:
+                    continue
+            else:
+                continue
+    print(f"No conflicts between {block1.block} and {block2.block}")
+    return
